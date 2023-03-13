fc76d50b58a3edecc06466d1d82d2ae0367af60f
phillee20 <phillee5647@hotmail.com>
2023-03-13 13:20:25 +0000

    Nav bar set up

--- nc-news/src/App.js
+++ nc-news/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+
+import "./App.css";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Topics from "./components/Topics";
+import Articles from "./components/Articles";
+import Users from "./components/Users";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header className="Header" />
+      <Nav className="Nav" />"
+      <Routes>
+        <Route path="/" element={<Articles />} />
+        <Route path="/Topics" element={<Topics />} />
+        <Route path="/Users" element={<Users />} />
+      </Routes>
     </div>
   );
 }
